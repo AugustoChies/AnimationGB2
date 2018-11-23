663f194421636c4351f681e20d2c9ccdc0c0346a
AugustoChies <guto_chies@hotmail.com>
2018-11-23 08:43:56 -0200

    project upload

---
+++ Assets/Node.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Node {
+
+    public bool walkable;
+    public Vector3 worldPosition;
+
+    public Node(bool w, Vector3 wP)
+    {
+        walkable = w;
+        worldPosition = wP;
+    }
+}
